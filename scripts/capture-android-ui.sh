fbe94f1647104c700cd15e500cacc0e985b92417
madowaku <67355875+madowaku@users.noreply.github.com>
2026-09-04 20:21:04 +0900

    test: add companion visual QA

--- scripts/capture-android-ui.sh
+++ scripts/capture-android-ui.sh
@@ -37,6 +37,7 @@ capture PAUSED 360 800 paused-360x800
 capture END_CONFIRM 360 800 end-confirm-360x800
 capture ABORTED 360 800 aborted-360x800
 capture VICTORY 360 800 victory-360x800
+capture COMPANION 360 800 companion-360x800
 capture LOG 360 800 log-360x800
 capture READY 720 1280 ready-720x1280
 capture CUSTOM 720 1280 custom-720x1280
@@ -46,6 +47,7 @@ capture PAUSED 720 1280 paused-720x1280
 capture END_CONFIRM 720 1280 end-confirm-720x1280
 capture ABORTED 720 1280 aborted-720x1280
 capture VICTORY 720 1280 victory-720x1280
+capture COMPANION 720 1280 companion-720x1280
 capture LOG 720 1280 log-720x1280
 
 python3 - <<'PY'
@@ -61,6 +63,7 @@ expected = {
     "end-confirm-360x800.png": (360, 800),
     "aborted-360x800.png": (360, 800),
     "victory-360x800.png": (360, 800),
+    "companion-360x800.png": (360, 800),
     "log-360x800.png": (360, 800),
     "ready-720x1280.png": (720, 1280),
     "custom-720x1280.png": (720, 1280),
@@ -70,6 +73,7 @@ expected = {
     "end-confirm-720x1280.png": (720, 1280),
     "aborted-720x1280.png": (720, 1280),
     "victory-720x1280.png": (720, 1280),
+    "companion-720x1280.png": (720, 1280),
     "log-720x1280.png": (720, 1280),
 }
 
