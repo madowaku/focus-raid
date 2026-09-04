189b9261f2e3d137e8319dc3ac3939e99a739f37
madowaku <67355875+madowaku@users.noreply.github.com>
2026-09-04 15:15:58 +0900

    test: simplify active notification detection

--- scripts/test-timer-durability.sh
+++ scripts/test-timer-durability.sh
@@ -31,7 +31,7 @@ clear_state() {
 
 notification_present() {
   adb shell dumpsys notification --noredact 2>/dev/null | tr -d '\r' | \
-    grep -Eq "NotificationRecord\\(.*pkg=${PACKAGE}|[[:space:]]0\\|${PACKAGE}\\|2500\\|"
+    grep -Fq "0|${PACKAGE}|2500|"
 }
 
 assert_no_notification() {
